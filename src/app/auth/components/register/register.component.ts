95a710453108256a74c5596e31f5dd935da0bbbb
Alex Ponce <alexponce2753@gmail.com>
2024-10-08 23:33:00 -0500

    se desarrollo el ingreso de datos de registro para que los datos vayan dirigido a la base de datos

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,15 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+
   registerForm!: FormGroup;
 
-  constructor(private fb: FormBuilder){}
+
+  constructor(private fb: FormBuilder,
+    private authService: AuthService,
+    private message: NzMessageService,
+    private router: Router
+  ){}
 
 
   ngOnInit(): void {
@@ -18,6 +27,17 @@ export class RegisterComponent {
     password:[null, Validators.required],
     name:[null, Validators.required]
    })
+  }
+
+  submitForm(){
+    this.authService.register(this.registerForm.value).subscribe(res =>{
+      if(res.id != null){
+        this.message.success("Registration successful", {nzDuration: 5000});
+        this.router.navigateByUrl("/");
+      }else{
+        this.message.error(`${res.message}`,{nzDuration:5000})
+      }
+    })
   }
 
 }
